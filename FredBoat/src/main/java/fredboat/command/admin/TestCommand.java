e260350b58299d9b92008218e55542fed2991b13
dotdog20 <sazjunsystems@gmail.com>
2017-01-08 15:29:49 -0700

    remove owner limit on ;;Test

--- FredBoat/src/main/java/fredboat/command/admin/TestCommand.java
+++ FredBoat/src/main/java/fredboat/command/admin/TestCommand.java
@@ -43,7 +43,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class TestCommand extends Command implements ICommandOwnerRestricted {
+public class TestCommand extends Command {
 
     @Override
     public void onInvoke(Guild guild, TextChannel channel, Member invoker, Message message, String[] args) {
