c9e62c0aa01e70eb1a9c1f819b28fb778384a185
dotdog20 <sazjunsystems@gmail.com>
2017-01-12 14:41:16 -0700

    yes

--- FredBoat/src/main/java/fredboat/command/fun/SergalCommand.java
+++ FredBoat/src/main/java/fredboat/command/fun/SergalCommand.java
@@ -44,8 +44,8 @@ import java.util.regex.Pattern;
 
 public class SergalCommand extends Command {
 
-    private static final Pattern IMAGE_PATTERN = Pattern.compile("\"file_url\":\"https://static1.e621.net/\"");
-    private static final String BASE_URL = "https://www.e621.net/post/index.html?tags=sergal,order:random,rating:s&limit=1/";
+    private static final Pattern IMAGE_PATTERN = Pattern.compile("\"file_url\":\"([^\"]+)");
+    private static final String BASE_URL = "https://www.e621.net/post/index.json?tags=sergal,order:random,rating:s&limit=1/";
 
     @Override
     public void onInvoke(Guild guild, TextChannel channel, Member invoker, Message message, String[] args) {
