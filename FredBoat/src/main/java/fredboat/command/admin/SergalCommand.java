f57a90e6f243d15f4ead7292f6159187f8a60356
dotdog20 <sazjunsystems@gmail.com>
2017-01-12 21:50:37 -0700

    warn 'em

--- FredBoat/src/main/java/fredboat/command/admin/SergalCommand.java
+++ FredBoat/src/main/java/fredboat/command/admin/SergalCommand.java
@@ -69,7 +69,7 @@ public class SergalCommand extends Command {
 
             File tmp = CacheUtil.getImageFromURL(m.group(1));
             channel.sendFile(tmp, null).queue();
-            log.info("IMG URL:" + m.group(1));
+            log.warn("IMG URL:" + m.group(1));
         } catch (UnirestException e) {
             channel.sendMessage("Failed to connect to " + BASE_URL).queue();
         } catch (IOException e) {
