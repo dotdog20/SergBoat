1db22e6a312ef4ef20342d93f0a3e9b268f93767
dotdog20 <sazjunsystems@gmail.com>
2017-01-12 21:42:39 -0700

    main import

--- FredBoat/src/main/java/fredboat/command/fun/SergalCommand.java
+++ FredBoat/src/main/java/fredboat/command/fun/SergalCommand.java
@@ -42,6 +42,7 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import fredboat.FredBoat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.commons.io.FileUtils;
