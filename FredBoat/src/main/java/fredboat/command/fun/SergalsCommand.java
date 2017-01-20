237564d6ea3218b2d5af12221d2088d1a02a8c1d
dotdog20 <sazjunsystems@gmail.com>
2017-01-20 16:23:54 -0700

    }

--- FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
+++ FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
@@ -66,6 +66,7 @@ public class SergalsCommand extends Command implements ICommandOwnerRestricted {
               channel.sendFile(tmp, null).queue();
             else
               channel.sendMessage("NOT-OWNER").queue();
+            }
         } catch (UnirestException e) {
             channel.sendMessage("Failed to connect to " + BASE_URL).queue();
         } catch (IOException e) {
