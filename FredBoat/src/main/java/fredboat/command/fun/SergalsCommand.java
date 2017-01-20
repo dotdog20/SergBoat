1fa29463dc1fc8aaef742a610c6ea28fe9ea0a73
dotdog20 <sazjunsystems@gmail.com>
2017-01-20 15:50:21 -0700

    l;

--- FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
+++ FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
@@ -60,7 +60,7 @@ public class SergalsCommand extends Command implements ICommandOwnerRestricted {
                 channel.sendMessage("INFO:" + str + m).queue();
                 return;
             }
-            if invoker.isUserBotCommander()
+            if(invoker.isUserBotCommander());
               File tmp = CacheUtil.getImageFromURL(m.group(1));
               channel.sendFile(tmp, null).queue();
             else
