d013cc54b14f6972bf13a547883699fa1fd7880a
dotdog20 <sazjunsystems@gmail.com>
2017-01-26 14:35:14 -0700

    Revert "javo"
    This reverts commit c568abdbf2ef87f522a8006cd216ab1b42729c14.

--- FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
+++ FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
@@ -61,13 +61,10 @@ public class SergalsCommand extends Command implements ICommandOwnerRestricted {
                 channel.sendMessage("INFO:" + str + m).queue();
                 return;
             }
-            List<Role> roles = guild.getMember(user).getRoles();
-            for (Role r : roles) {
-                if (r.getName().equals("Owner")) {
-                  File tmp = CacheUtil.getImageFromURL(m.group(1));
-                  channel.sendFile(tmp, null).queue();
-                }
-
+            if (DiscordUtil.isUserBotCommander()){
+              File tmp = CacheUtil.getImageFromURL(m.group(1));
+              channel.sendFile(tmp, null).queue();
+            }
               else {
                 channel.sendMessage("NOT-OWNER").queue();
               }
