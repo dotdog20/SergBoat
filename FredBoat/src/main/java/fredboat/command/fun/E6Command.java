e06d5c7bd517fada5c78e1170dea81762f220b9e
dotdog20 <sazjunsystems@gmail.com>
2017-02-08 15:38:31 -0700

    GP catch

--- FredBoat/src/main/java/fredboat/command/fun/E6Command.java
+++ FredBoat/src/main/java/fredboat/command/fun/E6Command.java
@@ -58,7 +58,7 @@ public class E6Command extends Command {
         //channel.sendMessage(finalString).queue();
         //File tmp = CacheUtil.getImageFromURL(finalString);
         //channel.sendFile(tmp, null).queue();
-      } catch (UnirestException e) {
+      } catch (Exception e) {
           channel.sendMessage("Failed to connect to " + BASE_URL).queue();
       }
     }
