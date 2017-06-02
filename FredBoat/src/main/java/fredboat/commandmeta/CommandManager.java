249d4dd20df5237dc438a551f6e13f3ece13fb16
dotdog20 <sazjunsystems@gmail.com>
2017-06-02 11:18:06 -0700

    check 21

--- FredBoat/src/main/java/fredboat/commandmeta/CommandManager.java
+++ FredBoat/src/main/java/fredboat/commandmeta/CommandManager.java
@@ -83,7 +83,7 @@ public class CommandManager {
         }
 
         if (invoked instanceof IMusicCommand && guild.getId().equals("320237845254111234")) {
-          channel.sendMessage(invoker.getEffectiveName() + "is Disabled.").queue();
+          channel.sendMessage(invoker.getEffectiveName() + "//").queue();
           return;
         }
         //Hardcode music commands in FredBoatHangout. Blacklist any channel that isn't #general or #staff, but whitelist Frederikam
