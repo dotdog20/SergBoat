4ba420978bf6c548091c5fedcee505134977a357
dotdog20 <sazjunsystems@gmail.com>
2017-06-02 13:34:24 -0700

    Import Disabled

--- FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
+++ FredBoat/src/main/java/fredboat/command/fun/SergalsCommand.java
@@ -37,6 +37,7 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
+import fredboat.commandmeta.abs.ICommandDisabled;
 
 import fredboat.util.DiscordUtil;
 import java.io.File;
