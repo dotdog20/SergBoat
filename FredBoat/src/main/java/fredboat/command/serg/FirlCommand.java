99fb6612b1f9aae41e4d02e6eb1f41c16460ed94
dotdog20 <sazjunsystems@gmail.com>
2017-07-29 08:52:57 -0700

    Add Firl first pass

---
+++ FredBoat/src/main/java/fredboat/command/serg/FirlCommand.java
@@ -0,0 +1,101 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Frederik Ar. Mikkelsen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package fredboat.command.serg;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import fredboat.Config;
+import fredboat.commandmeta.abs.Command;
+import fredboat.commandmeta.abs.IFunCommand;
+import fredboat.util.rest.CacheUtil;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import fredboat.FredBoat;
+import fredboat.feature.I18n;
+
+import org.slf4j.LoggerFactory;
+import fredboat.util.TextUtils;
+
+
+public class FirlCommand extends Command {
+
+    private static final Pattern IMAGE_PATTERN = Pattern.compile("\"url\":\"([^\"]+)");
+    private static final String BASE_URL = "https://www.reddit.com/r/furry_irl/random.json?limit=1";
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(E9Command.class);
+
+    @Override
+    public void onInvoke(Guild guild, TextChannel channel, Member invoker, Message message, String[] args) {
+        channel.sendTyping().queue();
+
+        try {
+
+            String str = Unirest.get(finalstring).asString().getBody();;
+            Matcher m = IMAGE_PATTERN.matcher(str);
+
+            if (!m.find()) {
+                //channel.sendMessage(MessageFormat.format(I18n.get(guild).getString("e926Fail"), BASE_URL)).queue();
+                //log.info("str: " + m);
+                log.info("finalsearchstring: " + finalstring);
+                //channel.sendMessage(str).queue();
+                //channel.sendMessage(finalstring).queue();
+                return;
+            }
+
+            File tmp = CacheUtil.getImageFromURL(m.group(1));
+            channel.sendFile(tmp, null).queue();
+            //log.info("///////////////////" + m.group(1) + "////////////////////////////////");
+            //channel.sendMessage(m.group(1)).queue();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        catch (UnirestException e) {
+          throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String help(Guild guild) {
+        return "{0}{1}\n#Gets a random post from /r/Furry_Irl";
+      }
+  }
