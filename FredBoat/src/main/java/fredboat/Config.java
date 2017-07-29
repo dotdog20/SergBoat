2ebc2384311df074fd7edd58e9a62141ca0a66f2
dotdog20 <sazjunsystems@gmail.com>
2017-07-28 18:02:18 -0700

    Read correct env var

--- FredBoat/src/main/java/fredboat/Config.java
+++ FredBoat/src/main/java/fredboat/Config.java
@@ -139,7 +139,7 @@ public class Config {
             cbKey = (String) creds.getOrDefault("cbKey", "");
             Map<String, String> token = (Map) creds.get("token");
             if (token != null) {
-                botToken = String.valueOf(System.getenv("PORT"));
+                botToken = String.valueOf(System.getenv("TOKEN"));
             } else botToken = "";
             spotifyId = (String) creds.getOrDefault("spotifyId", "");
             spotifySecret = (String) creds.getOrDefault("spotifySecret", "");
