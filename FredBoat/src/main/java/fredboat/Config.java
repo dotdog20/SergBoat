ed9e16822ae74ed3a13344793ddae6b25c2a89c3
dotdog20 <sazjunsystems@gmail.com>
2017-07-29 18:53:41 -0700

    api test 2

--- FredBoat/src/main/java/fredboat/Config.java
+++ FredBoat/src/main/java/fredboat/Config.java
@@ -152,7 +152,7 @@ public class Config {
 
             Object gkeys = creds.get("googleServerKeys");
             if (gkeys instanceof List) {
-                ((List) gkeys).forEach((Object str) -> googleKeys.add(String.valueOf(System.getenv("GOOGLEKEY")));
+                ((List) gkeys).forEach((Object str) -> googleKeys.add(String.valueOf(System.getenv("GOOGLEKEY"))));
             } else if (gkeys instanceof String) {
                 googleKeys.add(String.valueOf(System.getenv("GOOGLEKEY")));
                 log.info(String.valueOf(System.getenv("/////////////////////////////////////////////////////////////////////////////////////////////GOOGLEKEY")));
