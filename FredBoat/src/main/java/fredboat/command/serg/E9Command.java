043f2ec6e51efd0a6772b6188e9f5d0132275669
dotdog20 <sazjunsystems@gmail.com>
2017-07-29 17:49:14 -0700

    remove filesize limit for testing

--- FredBoat/src/main/java/fredboat/command/serg/E9Command.java
+++ FredBoat/src/main/java/fredboat/command/serg/E9Command.java
@@ -74,7 +74,7 @@ public class E9Command extends Command {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(BASE_URL);
             stringBuilder.append(args[1]);
-            stringBuilder.append(",order:random,filesize:0..8mb&limit=1&login=dotdot20&password_hash=");
+            stringBuilder.append(",order:random,&limit=1&login=dotdot20&password_hash=");
             stringBuilder.append(String.valueOf(System.getenv("E926_API")));
             String finalstring = stringBuilder.toString();
 
